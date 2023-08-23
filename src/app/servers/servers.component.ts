2c2a2b9ee6c80d1069b0337cc2965b874c48d944
vishwas-mutthuraj <vishwas.mutthuraj@gmail.com>
2023-08-23 16:05:27 +0530

    propertybinding for button disable

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   // templateUrl: './servers.component.html',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInit() {
+    
+  }
 
 }
