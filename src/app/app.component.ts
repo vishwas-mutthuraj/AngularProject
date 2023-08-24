48a70e2b9073024d743346cb4d0cad0f2cdebf80
vishwas-mutthuraj <vishwas.mutthuraj@gmail.com>
2023-08-24 18:23:00 +0530

    Created two new components for splitting the Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-  h3 {
-    color: red;
-  }
-  `]
+  styleUrls: ['./app.component.css']
+  // styles: [`
+  // h3 {
+  //   color: red;
+  // }
+  // `]
 })
 export class AppComponent {
+  serverElements = [];
+  // newServerName = '';
+  // newServerContent = '';
+
+  // onAddServer() {
+  //   this.serverElements.push({
+  //     type: 'server',
+  //     name: this.newServerName,
+  //     content: this.newServerContent
+  //   });
+  // }
+
+  // onAddBlueprint() {
+  //   this.serverElements.push({
+  //     type: 'blueprint',
+  //     name: this.newServerName,
+  //     content: this.newServerContent
+  //   });
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
