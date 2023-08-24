535a678506301d7377dd9e8f455bd246a68aea72
vishwas-mutthuraj <vishwas.mutthuraj@gmail.com>
2023-08-24 13:15:06 +0530

    working on ngIf ngFor ngStyle ngClass

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -23,6 +25,8 @@ export class ServersComponent implements OnInit {
     
   }
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName)
     this.serverCreationStatus = 'Server was Created! Name is ' + this.serverName;
   }
 
